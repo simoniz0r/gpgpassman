3b5ebe4e3e621ac660650ce54b3cfc44a05d0dd7
simoniz0r <bnu.fantasma@gmail.com>
2017-06-04 15:14:14 -0500

    Added password input for zenityupdatescript function when sudo is needed

--- gpgpassman.sh
+++ gpgpassman.sh
@@ -5,8 +5,8 @@
 # Also with 'zenity', you can execuite 'gpgpassman dec' for direct access to decrypting passwords; can be used with a keybind.
 # Written by simonizor 3/22/2017 - http://www.simonizor.gq/linuxapps
 
-GPMVER="1.3.7"
-X="v1.3.7 - Added separate update function for zenity; when gpgpassman update check is ran through the GUI, all updating will be done through the GUI also (no more terminal window for updating when in GUI mode)."
+GPMVER="1.3.8"
+X="v1.3.8 - Added password input for zenityupdatescript function when sudo is needed."
 # ^^Remember to update this every release and do not move their position!
 SCRIPTNAME="$0"
 GPMDIR="$(< ~/.config/gpgpassman/gpgpassman.conf)"
@@ -75,7 +75,8 @@ runupdate () {
     if [ "$SCRIPTNAME" = "/usr/bin/gpgpassman" ]; then
         git clone https://github.com/simoniz0r/gpgpassman.git /tmp/gpgpassman
         if [ -f "/tmp/gpgpassman/gpgpassman.sh" ]; then
-            sudo rm -f /usr/bin/gpgpassman
+            SUDOPASS="$(zenity --password --title=gpgpassman --text="Enter your password in order to update gpgpassman:")"
+            echo "$SUDOPASS" | sudo -S rm -f /usr/bin/gpgpassman
             sudo mv /tmp/gpgpassman/gpgpassman.sh /usr/bin/gpgpassman
             rm -rf /tmp/gpgpassman
             sudo chmod +x /usr/bin/gpgpassman
